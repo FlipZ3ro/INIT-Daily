48105db92f155c1a9cc5f235f49a5d05b3b09e97
FlipZ3ro <tokenla15@gmail.com>
2024-05-31 00:29:34 +0700

    Update fee GAS

--- src/config/initia.ts
+++ src/config/initia.ts
@@ -13,7 +13,7 @@ const privateKeyBuffer = Buffer.from(PRIVATE_KEY, 'hex') // Convert the private 
 
 const lcd = new LCDClient('https://lcd.initiation-1.initia.xyz', {
   chainId: 'initiation-1',
-  gasPrices: '0.15uinit',
+  gasPrices: '0.15move/944f8dd8dc49f96c25fea9849f16436dcfa6d564eec802f3ef7f8b3ea85368ff',
   gasAdjustment: '2.0',
 })
 
